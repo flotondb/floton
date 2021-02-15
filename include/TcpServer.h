dcbea18ad586d6a06b56f94b3db8f82dd9763284
Josh Weinstein <jweinst1@berkeley.edu>
2021-02-15 01:22:58 -0800

    Revert "Revert "Basic Tcp Echo handling""

--- include/TcpServer.h
+++ include/TcpServer.h
@@ -4,44 +4,69 @@
 #include <thread>
 #include <mutex>
 #include <condition_variable>
+#include <queue>
 
-#ifdef _WIN32
-#    pragma comment (lib, "Ws2_32.lib")
-#    define WIN32_LEAN_AND_MEAN
-#    include <windows.h>
-#    include <winsock2.h>
-#    include <ws2tcpip.h>
-#else // _WIN32
-#    include <unistd.h> 
-#    include <sys/socket.h> 
-#    include <netinet/in.h> 
-#endif // !_WIN32
+#include "SocketFormat.h"
 
 
-class TcpConnection {
+class TcpThreadPool {
 public:
-	TcpConnection(int fd);
-	~TcpConnection();
+	TcpThreadPool(size_t threadCount, SocketFormat_fn_ptr handlerFn = SocketFormat_echo_text);
+	~TcpThreadPool();
+
+	void go();
+	void stop();
+	bool addConnection(tcp_socket_t sd);
+
+	bool isShuttingDown() const { return _shuttingDown.load(); }
 private:
-	int _fd;
+	void work();
+	bool handle(tcp_socket_t sd);
+
+	size_t _threadCount;
+	std::thread* _threads;
+	std::atomic<bool> _shuttingDown;
+	SocketFormat_fn_ptr _handlerFn;
+	std::queue<tcp_socket_t> _conns;
+	std::condition_variable _cond;
+	mutable std::mutex _mut;
 };
 
 
 struct TcpServerConfig {
 	unsigned short port;
 	int backLogCount;
-	bool keepAlive;
+	size_t threadPoolSize;
+	SocketFormat_fn_ptr handlerFn;
 };
-
 
 class TcpServer {
 public:
 	TcpServer(const TcpServerConfig& config);
 	bool go();
+	void stop();
+
+	bool isShuttingDown() const { return _shuttingDown.load(); }
 private:
     TcpServerConfig _config;
+    TcpThreadPool _tpool;
+    std::atomic<bool> _shuttingDown;
     int _server_fd;
     sockaddr_in _address;
+};
+
+class TcpServerThread {
+public:
+	TcpServerThread(const TcpServerConfig& config);
+	void go();
+	void stop();
+	void join();
+
+	bool isShuttingDown() const { return _shuttingDown.load(); }
+private:
+	std::atomic<bool> _shuttingDown;
+	TcpServer _server;
+	std::thread _thread;
 };
 
 #endif // FLOTON_TCP_SERVER_H
