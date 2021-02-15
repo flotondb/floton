aa402218acc523d41307c865db1dd964982514ee
Josh Weinstein <jweinst1@berkeley.edu>
2021-02-15 01:21:35 -0800

    Revert "Basic Tcp Echo handling"

--- include/TcpServer.h
+++ include/TcpServer.h
@@ -4,69 +4,44 @@
 #include <thread>
 #include <mutex>
 #include <condition_variable>
-#include <queue>
 
-#include "SocketFormat.h"
+#ifdef _WIN32
+#    pragma comment (lib, "Ws2_32.lib")
+#    define WIN32_LEAN_AND_MEAN
+#    include <windows.h>
+#    include <winsock2.h>
+#    include <ws2tcpip.h>
+#else // _WIN32
+#    include <unistd.h> 
+#    include <sys/socket.h> 
+#    include <netinet/in.h> 
+#endif // !_WIN32
 
 
-class TcpThreadPool {
+class TcpConnection {
 public:
-	TcpThreadPool(size_t threadCount, SocketFormat_fn_ptr handlerFn = SocketFormat_echo_text);
-	~TcpThreadPool();
-
-	void go();
-	void stop();
-	bool addConnection(tcp_socket_t sd);
-
-	bool isShuttingDown() const { return _shuttingDown.load(); }
+	TcpConnection(int fd);
+	~TcpConnection();
 private:
-	void work();
-	bool handle(tcp_socket_t sd);
-
-	size_t _threadCount;
-	std::thread* _threads;
-	std::atomic<bool> _shuttingDown;
-	SocketFormat_fn_ptr _handlerFn;
-	std::queue<tcp_socket_t> _conns;
-	std::condition_variable _cond;
-	mutable std::mutex _mut;
+	int _fd;
 };
 
 
 struct TcpServerConfig {
 	unsigned short port;
 	int backLogCount;
-	size_t threadPoolSize;
-	SocketFormat_fn_ptr handlerFn;
+	bool keepAlive;
 };
+
 
 class TcpServer {
 public:
 	TcpServer(const TcpServerConfig& config);
 	bool go();
-	void stop();
-
-	bool isShuttingDown() const { return _shuttingDown.load(); }
 private:
     TcpServerConfig _config;
-    TcpThreadPool _tpool;
-    std::atomic<bool> _shuttingDown;
     int _server_fd;
     sockaddr_in _address;
-};
-
-class TcpServerThread {
-public:
-	TcpServerThread(const TcpServerConfig& config);
-	void go();
-	void stop();
-	void join();
-
-	bool isShuttingDown() const { return _shuttingDown.load(); }
-private:
-	std::atomic<bool> _shuttingDown;
-	TcpServer _server;
-	std::thread _thread;
 };
 
 #endif // FLOTON_TCP_SERVER_H
--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,29 +1,7 @@
 #include <cstdio>
-#include <signal.h>
-#include "TcpServer.h"
-
-static TcpServerThread* TCP_SERV_THREAD = nullptr;
-
-void interSigHandler(int dummy)
-{
-	(void)dummy;
-	TCP_SERV_THREAD->stop();
-	std::puts("Shutting Down due to interrupt signal");
-	exit(0);
-}
 
 int main(int argc, char const *argv[])
 {
-	TcpServerConfig config;
-	signal(SIGINT, interSigHandler);
-	std::puts("The Floton Database");
-	std::puts("------------------------------");
-	config.port = 23000;
-	config.backLogCount = 500;
-	config.threadPoolSize = 50;
-	std::printf("Serving requests on port: %u\n", config.port);
-	TCP_SERV_THREAD = new TcpServerThread(std::move(config));
-	TCP_SERV_THREAD->go();
-	TCP_SERV_THREAD->join();
+	std::puts("Floton");
 	return 0;
 }
