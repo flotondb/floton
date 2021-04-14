b954c862c70c7b66bc323e49a70b3cf196d9c1a4
Joshua Weinstein <jweinstein@splunk.com>
2021-04-14 01:09:37 -0700

    cache work

--- include/AtomicCache.h
+++ include/AtomicCache.h
@@ -33,12 +33,54 @@ public:
 		       _data(new std::atomic<T>[_size])
 		{}
 
+		~node()
+		{
+			delete[] _enqBarrier;
+			delete[] _deqBarrier;
+			delete[] _data;
+		}
+
+		bool inline recycle() {
+			bool claimedByte = false;
+			if (_recycling.compare_exchange_strong(claimedByte, true)) {
+				// Only one thread succeeds here.
+				_enqState.store(0);
+				_deqState.store(0);
+				for (size_t i = 0; i < ATOMIC_CACHE_BARR_SIZE(_size); ++i)
+				{
+					_enqBarrier[i] = 0;
+					_deqBarrier[i] = 0;
+				}
+				_recycling.store(false);
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		bool inline isFull() const {
+			for (size_t i = 0; i < ATOMIC_CACHE_BARR_SIZE(_size); ++i)
+			{
+				if (_deqBarrier[i].load() != 0xFFFFFFFFFFFFFFFF)
+					return false;
+			}
+			return true;
+		}
+
 		void inline setEnqBarrier(uint64_t index) {
-			_enqBarrier[index / 64].fetch_xor(index % 64);
+			_enqBarrier[index / 64].fetch_xor(1 << (index % 64));
+		}
+
+		bool inline checkEnqBarrier(uint64_t index) {
+			return _enqBarrier[index / 64] & (1 << (index % 64));
 		}
 
 		void inline setDeqBarrier(uint64_t index) {
-			_deqBarrier[index / 64].fetch_xor(index % 64);
+			_deqBarrier[index / 64].fetch_xor(1 << (index % 64));
+		}
+
+		bool inline checkDeqBarrier(uint64_t index) {
+			return _deqBarrier[index / 64] & (1 << (index % 64));
 		}
 	};
 
@@ -46,9 +88,34 @@ public:
 	                                      _nextNodeSize(startingNodeSize)
 	                                      {}
 
-	void give(const T& object);
+	void give(const T& object)
+	{
+		node* markNode = _marker.load();
+		if (markNode->isFull()) {
+			if (markNode->recycle()) {
 
-	bool take(T& object);
+			} else {
+
+			}
+		}
+		uint64_t markEnq = markNode->_enqState.fetch_add(1);
+		while (markEnq >= markNode->_size) {
+
+		}
+		markNode->_data[markEnq].store(object);
+		markNode->setEnqBarrier(markEnq);
+	}
+
+	bool take(T& object)
+	{
+		return true;
+	}
+
+	void inline increaseNodeSize(size_t factor)
+	{
+		size_t currNodeSize = _nextNodeSize.load();
+		_nextNodeSize.compare_exchange_strong(currNodeSize, currNodeSize * factor);
+	}
 private:
 	std::atomic<node*> _marker;
 	std::atomic<size_t> _nextNodeSize;
